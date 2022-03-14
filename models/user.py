c58291cb27d390c2f3fd3636026ab1a149aa2e23
kate <kateincoding@gmail.com>
2022-03-13 23:12:48 -0500

    [user table done]

--- models/user.py
+++ models/user.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
 """Module for User"""
+from models.basemodel import Basemodel, Basemodel
+from sqlalchemy import DateTime
+from sqlalchemy.sql.schema import Column ForeignKey
+from sqlalchemy.sql.sqltypes import String, Integer
+
+from sqlalchemy.orm import relationship, backref
+from os import getenv
+
+class User(Basemodel, Base):
+    """class for users"""
+    __tablename__ = 'users'
+    first_name = Column(String(30))
+    last_name = Column(String(30))
+    whatsapp = Column(Integer(15), nullable=False)
+    email = Column(String(30))
+    num_entradas = COlumn(Integer(2), default=1)
+    confirmation = Column(Integer(2), default=0)
